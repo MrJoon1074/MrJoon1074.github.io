77929c49b34e74c25e9a2aeac89cb6e69f0ab8c5
Joon <joon.han@richmond.edu>
2022-12-10 00:20:40 -0500

    a

---
+++ BOAT/js/script.js
@@ -0,0 +1,10 @@
+document.getElementById("seller-button").addEventListener("click", myFunction);
+document.getElementById("min").addEventListener("click", myFunction2);
+function myFunction() {
+
+  document.getElementById("big_container").style.visibility="visible"
+}
+function myFunction2() {
+
+  document.getElementById("big_container").style.visibility="hidden"
+}
